692d8f29f4d5dd04edec2aaa12d589ce8fa71aa7
rmancan <rafael.mancan@gmail.com>
2021-08-21 14:51:41 -0300

    updating 21/08/2021 14:51:41

--- .bkp-dot.sh
+++ .bkp-dot.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-exec > /dev/null
-
 cp ~/.zshrc ~/dotfiles/
 
 cp -r ~/.config/kitty ~/dotfiles/
-cp -r ~/.config/nvim/ ~/dotfiles/neovim/
+cp -r ~/.config/nvim/ ~/dotfiles/neovim/ 2>/dev/null
 cp ~/.tmux.conf.local ~/dotfiles/.tmux.conf.local
 cp ~/.gitconfig ~/dotfiles/.gitconfig
 
