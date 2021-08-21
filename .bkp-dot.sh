6bdfbbf1b53265152c97e6d76ab3aec40f8fd2cd
rmancan <rafael.mancan@gmail.com>
2021-08-21 14:47:10 -0300

    updating 21/08/2021 14:47:10

--- .bkp-dot.sh
+++ .bkp-dot.sh
@@ -2,21 +2,13 @@
 cp ~/.zshrc ~/dotfiles/
 
 cp -r ~/.config/kitty ~/dotfiles/
-cp -r ~/.config/nvim/general ~/dotfiles/neovim/
-cp -r ~/.config/nvim/keys ~/dotfiles/neovim/
-cp -r ~/.config/nvim/lua ~/dotfiles/neovim/
-cp -r ~/.config/nvim/plug-config ~/dotfiles/neovim/
-cp -r ~/.config/nvim/snips ~/dotfiles/neovim/
-cp -r ~/.config/nvim/themes ~/dotfiles/neovim/
-cp -r ~/.config/nvim/vim-plug ~/dotfiles/neovim/
-cp ~/.config/nvim/init.vim ~/dotfiles/neovim/init.vim
-cp ~/.config/nvim/coc-settings.json ~/dotfiles/neovim/coc-settings.json
+cp -r ~/.config/nvim/ ~/dotfiles/neovim/
 cp ~/.tmux.conf.local ~/dotfiles/.tmux.conf.local
 cp ~/.gitconfig ~/dotfiles/.gitconfig
 
 cp ~/.bkp-dot.sh ~/dotfiles/
 
-cd ~/dotfiles
+cd ~/dotfiles || exit
 git add .
 dt=$(date '+%d/%m/%Y %H:%M:%S')
 git commit -m "updating $dt"
