8da55e400d451eb6c460e105c87344bb945b25a1
rmancan <rafael.mancan@gmail.com>
2021-12-22 14:28:33 -0300

    updating 22/12/2021 14:28:33

--- neovim/lua/user/plugins.lua
+++ neovim/lua/user/plugins.lua
@@ -81,6 +81,7 @@ return packer.startup(function(use)
   use "hrsh7th/cmp-cmdline" -- cmdline completions
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
   use "hrsh7th/cmp-nvim-lsp"
+  use "github/copilot.vim"
 
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
