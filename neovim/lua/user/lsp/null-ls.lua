062a80a03f1168789a10e87923a7ca8f6e015b3f
rmancan <rafael.mancan@gmail.com>
2022-01-13 11:03:05 -0300

    updating 13/01/2022 11:03:05

--- neovim/lua/user/lsp/null-ls.lua
+++ neovim/lua/user/lsp/null-ls.lua
@@ -11,10 +11,8 @@ local diagnostics = null_ls.builtins.diagnostics
 null_ls.setup({
 	debug = false,
 	sources = {
-		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
-		formatting.black.with({ extra_args = { "--fast" } }),
-		formatting.stylua,
     formatting.phpcsfixer,
-    diagnostics.eslint,
+		formatting.prettier,
+		formatting.stylua,
 	},
 })
