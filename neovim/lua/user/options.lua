c045e186b3a4a09ed3664f1eb7931e412a438e82
rmancan <rafael.mancan@gmail.com>
2022-01-14 14:57:23 -0300

    updating 14/01/2022 14:57:23

--- neovim/lua/user/options.lua
+++ neovim/lua/user/options.lua
@@ -44,6 +44,3 @@ end
 vim.cmd "set whichwrap+=<,>,[,],h,l"
 vim.cmd [[set iskeyword+=-]]
 vim.cmd [[set formatoptions-=cro]] -- TODO: this doesn't seem to work
-
-vim.cmd('autocmd ColorScheme * highlight Normal guibg=NONE ctermbg=NONE')
-vim.cmd('autocmd ColorScheme * highlight NonText guibg=NONE ctermbg=NONE')
