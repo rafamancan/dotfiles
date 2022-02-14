215b36e6ef290c4a432e3f39d8116a4b9dacef5e
rmancan <rafael.mancan@gmail.com>
2022-02-14 15:25:18 -0300

    updating 14/02/2022 15:25:18

--- neovim/lua/user/colorscheme.lua
+++ neovim/lua/user/colorscheme.lua
@@ -1,8 +1,20 @@
-vim.cmd [[
+vim.cmd([[
 try
-  colorscheme darkplus
+  let g:gruvbox_contrast_dark = 'hard'
+  let g:gruvbox_invert_selection = '0'
+  colorscheme gruvbox
+  highlight ColorColumn ctermbg=0 guibg=grey
+  hi SignColumn guibg=none
+  hi CursorLineNR guibg=None
+  highlight Normal guibg=none
+  " highlight LineNr guifg=#ff8659
+  " highlight LineNr guifg=#aed75f
+  highlight LineNr guifg=#5eacd3
+  highlight netrwDir guifg=#5eacd3
+  highlight qfFileName guifg=#aed75f
+  hi TelescopeBorder guifg=#5eacd
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
 endtry
-]]
+]])
