112914bcb081313f579b88abc63c5c3a7b336289
Rafael Mançan <rafael.mancan@gmail.com>
2022-08-12 10:06:25 -0300

    updating 12/08/2022 10:06:25

--- lunarvim/config.lua
+++ lunarvim/config.lua
@@ -51,13 +51,23 @@ lvim.plugins = {
     cmd = "TroubleToggle",
   },
   { "wakatime/vim-wakatime" },
-  {
-    "tzachar/cmp-tabnine",
-    run = "./install.sh",
-    requires = "hrsh7th/nvim-cmp",
-    event = "InsertEnter",
-  },
+  { "github/copilot.vim" },
 }
+
+-- copilot setup
+vim.g.copilot_no_tab_map = true
+vim.g.copilot_assume_mapped = true
+vim.g.copilot_tab_fallback = ""
+local cmp = require "cmp"
+lvim.builtin.cmp.mapping["<C-e>"] = function(fallback)
+    cmp.mapping.abort()
+    local copilot_keys = vim.fn["copilot#Accept"]()
+    if copilot_keys ~= "" then
+        vim.api.nvim_feedkeys(copilot_keys, "i", true)
+    else
+        fallback()
+    end
+end
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
 lvim.autocommands.custom_groups = {
