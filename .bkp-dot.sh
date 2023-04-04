a5298f65b24a69bfe3740b33dd1cb9bfe46659c4
Rafael Mançan <rafael.mancan@gmail.com>
2023-04-04 11:40:39 -0300

    updating 04/04/2023 11:40:39

--- .bkp-dot.sh
+++ .bkp-dot.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 cp ~/.zshrc ~/dotfiles/
 
-cp -r ~/.config/nvim/ ~/dotfiles/neovim/ 2>/dev/null
-cp -r ~/.config/lvim/config.lua ~/dotfiles/lunarvim/ 2>/dev/null
+# cp -r ~/.config/nvim/ ~/dotfiles/neovim/ 2>/dev/null
 # cp ~/.config/starship.toml ~/dotfiles/
 # cp ~/.tmux.conf.local ~/dotfiles/.tmux.conf.local 2>/dev/null
+
+cp -r ~/.config/lvim/config.lua ~/dotfiles/lunarvim/ 2>/dev/null
 cp ~/.gitconfig ~/dotfiles/.gitconfig
 cp ~/wterm-keys.json ~/dotfiles/wterm-keys.json 2>/dev/null
 
