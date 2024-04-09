df542729196ce6979a0c54133e4bacc196e8d244
Rafael Mançan <rafael.mancan@gmail.com>
2024-04-09 10:25:52 -0300

    updating 09/04/2024 10:25:52

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -116,4 +116,13 @@ return {
       "nvim-telescope/telescope.nvim",
     },
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = "cd app && npm install",
+    init = function()
+      vim.g.mkdp_filetypes = { "markdown" }
+    end,
+    ft = { "markdown" },
+  },
 }
