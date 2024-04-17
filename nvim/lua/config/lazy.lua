838ba3e856a8271961468d392c3271c31b8d8b6c
Rafael Mançan <rafael.mancan@gmail.com>
2024-04-17 10:07:26 -0300

    updating 17/04/2024 10:07:26

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -16,7 +16,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
-    { import = "lazyvim.plugins.extras.lang.tailwind" },
+    -- { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -67,10 +67,10 @@ return {
     opts = function(_, opts)
       table.insert(opts.sources, { name = "emoji" })
       local format_kinds = opts.formatting.format
-      opts.formatting.format = function(entry, item)
-        format_kinds(entry, item) -- add icons
-        return require("tailwindcss-colorizer-cmp").formatter(entry, item)
-      end
+      -- opts.formatting.format = function(entry, item)
+      --   format_kinds(entry, item) -- add icons
+      --   return require("tailwindcss-colorizer-cmp").formatter(entry, item)
+      -- end
     end,
   },
   {
