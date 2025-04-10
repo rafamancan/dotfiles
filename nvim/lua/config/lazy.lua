75ab18ae2115ac17a6ce27c2d072c53ec263986d
Rafael Mançan <rafael.mancan@gmail.com>
2025-04-10 14:40:13 -0300

    updating 10/04/2025 14:40:13

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -21,7 +21,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.json" },
     { import = "lazyvim.plugins.extras.lang.markdown" },
     { import = "lazyvim.plugins.extras.lang.go" },
-    { import = "lazyvim.plugins.extras.ai.copilot" },
+    -- { import = "lazyvim.plugins.extras.ai.copilot" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
     { import = "lazyvim.plugins.extras.lang.docker" },
