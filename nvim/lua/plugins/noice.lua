337b58811d7e83bebcd1254377c4f20e31163a5d
Rafael Mançan <rafael.mancan@gmail.com>
2025-11-25 19:17:30 -0300

    updating 25/11/2025 19:17:30

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -8,6 +8,11 @@ return {
   {
     "folke/noice.nvim",
     opts = {
+      lsp = {
+        hover = {
+          silent = true, -- Don't show "No information available" notification
+        },
+      },
       presets = {
         lsp_doc_border = true, -- Add border to hover docs and signature help
       },
